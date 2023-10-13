6688d6691136a9196cf673ab0018a2cadb2f8951
Ricbon Ibe <ibericbon8@gmail.com>
2023-10-14 05:10:43 +0800

    update

--- graphql_jwt/decorators.py
+++ graphql_jwt/decorators.py
@@ -98,30 +98,30 @@ def token_auth(f):
         agent = kwargs.get('agent')
 
         if agent and agent == 'true':
-            # user = get_user_model().objects.filter((Q(username__iexact=username) | Q(email__iexact=username)) & Q(agent__iexact=1)).first()
-            # if user:
-            #     if not user.check_password(password):
-            #         raise exceptions.JSONWebTokenError(
-            #             _("Please enter valid credentials"),
-            #         ) 
-            # else:
-            raise exceptions.JSONWebTokenError(
-                _("Please enter valid credentialsddd"),
-            )
+            user = get_user_model().objects.filter((Q(username__iexact=username) | Q(email__iexact=username)) & Q(agent__iexact=1)).first()
+            if user:
+                if not user.check_password(password):
+                    raise exceptions.JSONWebTokenError(
+                        _("Please enter valid credentials 1"),
+                    ) 
+            else:
+                raise exceptions.JSONWebTokenError(
+                    _("Please enter valid credentials 1"),
+                )
         else:
             user = get_user_model().objects.filter((Q(username__iexact=username) | Q(email__iexact=username)) & Q(agent__iexact=0)).first()
             if user:
                 if not user.check_password(password):
                     raise exceptions.JSONWebTokenError(
-                        _("Please enter valid credentials"),
+                        _("Please enter valid credentials 2"),
                     ) 
             else:
                 raise exceptions.JSONWebTokenError(
-                    _("Please enter valid credentials"),
+                    _("Please enter valid credentials 2"),
                 ) 
         if user is None:
             raise exceptions.JSONWebTokenError(
-                _("Please enter valid credentials"),
+                _("Please enter valid credentials 2"),
             )
 
         if hasattr(context, "user"):
